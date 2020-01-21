45f7cbd79cddac7790e7c1b301ea9d24c8984196
Alex Nim <alexnimjli@gmail.com>
2020-01-21 16:32:43 +0000

    preliminary pca code

--- 02 - Code/20200119 - 00 - Data Processing class.py
+++ 02 - Code/20200119 - 00 - Data Processing class.py
@@ -102,6 +102,47 @@ class DataProcessing:
 # -
 
 
+df.standardiser()
+
+
+
+
+
+
+
+
+
+
+
+from sklearn.decomposition import PCA 
+pca = PCA(n_components = 2)
+X2D = pca.fit_transform(X_train)
+
+pca.explained_variance_ratio_
+
+figure(num=None, figsize=(15, 8), dpi=80, facecolor='w', edgecolor='k')
+colors = ['red', 'green']
+plt.scatter(X2D[:, 0], X2D[:, 1], c=y_train, zorder=10, s=2, cmap=matplotlib.colors.ListedColormap(colors))
+
+
+pca = PCA()
+pca.fit(X_train)
+cumsum = np.cumsum(pca.explained_variance_ratio_) 
+d = np.argmax(cumsum >= 0.95) + 1
+
+d
+
+pca = PCA(n_components=0.95) 
+X_reduced = pca.fit_transform(X_train)
+
+
+
+
+
+
+
+>>>>>>> Stashed changes
+
 
 
 
