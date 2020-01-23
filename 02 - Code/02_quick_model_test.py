28178a65c28512881755c27159011a1fccba9f21
Alex Nim <alexnimjli@gmail.com>
2020-01-23 15:52:35 +0000

    cleaning up code

--- 02 - Code/02_quick_model_test.py
+++ 02 - Code/02_quick_model_test.py
@@ -69,8 +69,6 @@ def confusion_matrices(y, y_pred):
     return
 
 
-# # doing this with cross_validate
-
 # +
 GaussianNB = GaussianNB()
 SGDClassifier = SGDClassifier()
@@ -145,47 +143,3 @@ confusion_matrices(y_test, y_test_pred)
 
 
 
-
-# # doing this with cross_val_predict
-
-# scores_df = pd.DataFrame(columns = ['accuracy', 'precision', 'recall', 'f1', 'roc_auc', 'run_time'])
-#
-# models = [GaussianNB(), RandomForestClassifier(n_estimators=10), 
-#             SGDClassifier(), XGBClassifier()]
-# names = ["Naive Bayes", "Random Forest Classifier", "SGD Classifier", "XGBClassifier"]
-#
-# #models = [GaussianNB(), SGDClassifier()]
-# #names = ['Naive Bayes', 'SGD Classifier']
-#
-# for model, name in zip(models, names):
-#     temp_list = []
-#     print(name)
-#     start = time.time()
-#     
-#     y_train_pred = cross_val_predict(model, X_train, y_train, cv=10)
-#     
-#     for score in ["accuracy", "precision", "recall", "f1", 'roc_auc']:
-#         if score == 'accuracy':
-#             mean_score = accuracy_score(y_train, y_train_pred)
-#         elif score == 'precision':
-#             mean_score = precision_score(y_train, y_train_pred)          
-#         elif score == 'recall':
-#             mean_score = recall_score(y_train, y_train_pred)
-#         elif score == 'f1':
-#             mean_score = f1_score(y_train, y_train_pred)
-#         elif score == 'auc':
-#             mean_score = auc_roc_score(y_train, y_train_pred)
-#         
-#         print('{} mean : {}'.format(score, mean_score))
-#         temp_list.append(mean_score)
-#     
-#     #doing it this way takes more time, because it runs cross_val_score 4 different times
-#     #for score in ["accuracy", "precision", "recall", "f1"]:
-#      #   mean_score = cross_val_score(model, X_train, y_train, scoring=score, cv=10).mean()
-#      #   print('{} mean score: {}'.format(score, mean_score))
-#     
-#     
-#     temp_list.append(time.time() - start)
-#     print("time to run: {}".format(time.time() - start))
-#     print("\n")
-#     scores_df.loc[name] = temp_list
