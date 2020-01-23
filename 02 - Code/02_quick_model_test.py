6fcc44dafb660036a998eb64a5b9f5b6b4240a47
Alex Nim <alexnimjli@gmail.com>
2020-01-23 11:44:39 +0000

    adding XGBoost, roc_auc score and f1 score to test

--- 02 - Code/02_quick_model_test.py
+++ 02 - Code/02_quick_model_test.py
@@ -18,44 +18,68 @@ import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 
-X_train = pd.read_csv("../01 - Data/X_train.csv")
-y_train = pd.read_csv('../01 - Data/y_train.csv', header = None, index_col = 0, squeeze = bool)
-X_train = X_train.drop('Unnamed: 0', axis=1)
+X_train = pd.read_csv("../01 - Data/unbalanced_X_train.csv")
+#y_train = pd.read_csv('../01 - Data/unbalanced_y_train.csv', header = None, index_col = 0, squeeze = bool)
+#X_train = X_train.drop('Unnamed: 0', axis=1)
+y_train = pd.read_csv("../01 - Data/unbalanced_y_train.csv", header = None)
 # -
+
+import time
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, roc_auc_score
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from xgboost import XGBClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import cross_val_score
 
 X_train.shape
 
 y_train.shape
 
-
-
 # +
-import time
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score, recall_score, f1_score
-from sklearn.svm import SVC
-from sklearn.naive_bayes import GaussianNB
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.linear_model import SGDClassifier
+scores_df = pd.DataFrame(columns = ['accuracy', 'precision', 'recall', 'f1', 'roc_auc', 'run_time'])
 
-scores_df = pd.DataFrame(columns = ['accuracy', 'precision', 'recall', 'run_time'])
+models = [GaussianNB(), RandomForestClassifier(n_estimators=10), 
+            SGDClassifier(), DecisionTreeClassifier(), XGBClassifier()]
+names = ["Naive Bayes", "Random Forest Classifier", "SGD Classifier", "Decision Tree", "XGBClassifier"]
 
-models = [GaussianNB(), RandomForestClassifier(n_estimators=10), SGDClassifier(), DecisionTreeClassifier()]
-names = ["Naive Bayes", "Random Forest Classifier", "SGD Classifier", "Decision Tree"]
-
+#models = [GaussianNB(), SGDClassifier()]
+#names = ['Naive Bayes', 'SGD Classifier']
 
 for model, name in zip(models, names):
     temp_list = []
     print(name)
     start = time.time()
-    for score in ["accuracy", "precision", "recall"]:
-        mean_score = cross_val_score(model, X_train, y_train,scoring=score, cv=5).mean()
-        print("{} mean : {}".format(score, mean_score))
+    
+    y_train_pred = cross_val_predict(model, X_train, y_train, cv=10)
+    
+    for score in ["accuracy", "precision", "recall", "f1", 'roc_auc']:
+        if score == 'accuracy':
+            mean_score = accuracy_score(y_train, y_train_pred)
+        elif score == 'precision':
+            mean_score = precision_score(y_train, y_train_pred)          
+        elif score == 'recall':
+            mean_score = recall_score(y_train, y_train_pred)
+        elif score == 'f1':
+            mean_score = f1_score(y_train, y_train_pred)
+        elif score == 'auc':
+            mean_score = auc_roc_score(y_train, y_train_pred)
+        
+        print('{} mean : {}'.format(score, mean_score))
         temp_list.append(mean_score)
+    
+    #doing it this way takes more time, because it runs cross_val_score 4 different times
+    #for score in ["accuracy", "precision", "recall", "f1"]:
+     #   mean_score = cross_val_score(model, X_train, y_train, scoring=score, cv=10).mean()
+     #   print('{} mean score: {}'.format(score, mean_score))
+    
+    
     temp_list.append(time.time() - start)
     print("time to run: {}".format(time.time() - start))
     print("\n")
@@ -64,7 +88,9 @@ for model, name in zip(models, names):
 
 scores_df
 
-for i in ['accuracy', 'precision', 'recall', 'run_time']:
+scores_df
+
+for i in ['accuracy', 'precision', 'recall', 'f1', 'roc_auc', 'run_time']:
     scores_df.plot.bar(y = i) 
 
 
