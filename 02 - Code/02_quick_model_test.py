8d9d78d0b2209ca24e0ee81f01abd1250b37e2a5
Alex Nim <alexnimjli@gmail.com>
2020-01-23 13:06:25 +0000

    adding confusion matrix and alternative method with cross_validate

--- 02 - Code/02_quick_model_test.py
+++ 02 - Code/02_quick_model_test.py
@@ -37,17 +37,80 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_validate
+
+
+def confusion_matrices(y, y_pred):
+    y_pred = y_pred.round()
+    confusion_mat = confusion_matrix(y, y_pred)
+    sns.set_style("white")
+    plt.matshow(confusion_mat, cmap=plt.cm.gray)
+    plt.show()
+    row_sums = confusion_mat.sum(axis=1, keepdims=True)
+    normalised_confusion_mat = confusion_mat/row_sums
+    print(confusion_mat, "\n")
+    print(normalised_confusion_mat)
+    plt.matshow(normalised_confusion_mat, cmap=plt.cm.gray)
+    plt.show()
+    print('the precision score is : ', precision_score(y, y_pred))
+    print('the recall score is : ', recall_score(y, y_pred))
+    print('the f1 score is : ', f1_score(y, y_pred))
+    print('the accuracy score is : ', accuracy_score(y, y_pred))
+    return
+
 
 X_train.shape
 
 y_train.shape
 
+# # doing this with cross_validate
+
+# +
+scores_df = pd.DataFrame(columns = ['accuracy', 'precision', 'recall', 'f1', 'roc_auc', 'run_time'])
+
+models = [GaussianNB(), SGDClassifier()]
+names = ["Naive Bayes", "SGD Classifier"]
+
+for model, name in zip(models, names):
+    temp_list = []
+    print(name)
+    start = time.time()
+    
+    scores = cross_validate(model, X_train, y_train,
+                                  scoring=('accuracy', 'precision', 'recall', 'f1', 'roc_auc'),
+                                  return_train_score=True, cv=10)
+    
+    for score in ['accuracy', 'precision', 'recall', 'f1', 'roc_auc']:
+        if score == 'accuracy':
+            mean_score = scores['test_accuracy'].mean()
+        elif score == 'precision':
+            mean_score = scores['test_precision'].mean()       
+        elif score == 'recall':
+            mean_score = scores['test_recall'].mean()
+        elif score == 'f1':
+            mean_score = scores['test_f1'].mean()
+        elif score == 'auc':
+            mean_score = scores['test_roc_auc'].mean()
+        
+        print('{} mean : {}'.format(score, mean_score))
+        temp_list.append(mean_score)
+    
+    temp_list.append(time.time() - start)
+    
+    print("time to run: {}".format(time.time() - start))
+    print("\n")
+    scores_df.loc[name] = temp_list
+    
+# -
+
+# # doing this with cross_val_predict
+
 # +
 scores_df = pd.DataFrame(columns = ['accuracy', 'precision', 'recall', 'f1', 'roc_auc', 'run_time'])
 
 models = [GaussianNB(), RandomForestClassifier(n_estimators=10), 
-            SGDClassifier(), DecisionTreeClassifier(), XGBClassifier()]
-names = ["Naive Bayes", "Random Forest Classifier", "SGD Classifier", "Decision Tree", "XGBClassifier"]
+            SGDClassifier(), XGBClassifier()]
+names = ["Naive Bayes", "Random Forest Classifier", "SGD Classifier", "XGBClassifier"]
 
 #models = [GaussianNB(), SGDClassifier()]
 #names = ['Naive Bayes', 'SGD Classifier']
